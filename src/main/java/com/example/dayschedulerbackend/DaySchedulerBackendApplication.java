4c40b037b914521fa31669a1fc7196a916acb156
Alekseych4 <alekseych@list.ru>
2022-05-20 05:54:18 +0300

    Initial commit

---
+++ src/main/java/com/example/dayschedulerbackend/DaySchedulerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.dayschedulerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaySchedulerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DaySchedulerBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dayschedulerbackend/DaySchedulerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dayschedulerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaySchedulerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
